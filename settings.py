05c12764774ad473a7ba657d29152c6bdf9d31ac
infGAI <infgai@yandex.ru>
2023-12-29 15:35:04 +0300

    fix hero moving

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-WINDOW_SIZE = WINDOW_WIGHT, WINDOW_HEIGHT = 400, 1000
+WINDOW_SIZE = WINDOW_WIGHT, WINDOW_HEIGHT = 400, 600
 FPS = 40
 max_x = 11
 max_y = 11
 SIZE_TEXT = 30
-DIRECTIONS = (5, 7)
-A_XY = (0, -5)
+DIRECTIONS = (1, 3)
+A_XY = (0, -1)
 LEN_JUMP = 10
 tile_width = tile_height = 20
 HERO_IMAGE = "data/box.png"
